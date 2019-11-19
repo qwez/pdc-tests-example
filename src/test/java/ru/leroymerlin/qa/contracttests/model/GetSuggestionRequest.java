d26d4ed033a8d278053ce13c5e944d83e2049f39
Павел Коростин <pavel.korostin_ext@leroymerlin.ru>
2019-11-19 17:17:35 +0300

    fix

--- src/test/java/ru/leroymerlin/qa/contracttests/model/GetSuggestionRequest.java
+++ src/test/java/ru/leroymerlin/qa/contracttests/model/GetSuggestionRequest.java
@@ -2,7 +2,6 @@ package ru.leroymerlin.qa.contracttests.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import ru.leroymerlin.qa.contracttests.SuggestionControllerGetSuggestions;
 
 @AllArgsConstructor
 @Getter
